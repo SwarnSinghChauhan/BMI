8659974825d8cbfca4259fcf7e7bc91ee328f151
Swarn <swarnsinghchauhan563@gmail.com>
2026-01-21 19:03:07 +0530

    Revert auth configuration to fix AuthClientOptions error

--- SampleGoogleSignin/SupabaseClient.swift
+++ SampleGoogleSignin/SupabaseClient.swift
@@ -11,13 +11,5 @@ import Supabase
 let supabaseURL = URL(string:"https://wgitvtolkhutekqgnxux.supabase.co" )!
 let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnaXR2dG9sa2h1dGVrcWdueHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODY3OTUsImV4cCI6MjA4NDU2Mjc5NX0.lilHKacJyf3xdxHMdDg3j5-yW1HndJiT1xvvzg6iweY"
 
-let supabaseClient = SupabaseClient(
-    supabaseURL: supabaseURL,
-    supabaseKey: supabaseKey,
-    options: SupabaseClientOptions(
-        auth: AuthClientOptions(
-            emitLocalSessionAsInitialSession: true
-        )
-    )
-)
+let supabaseClient = SupabaseClient(supabaseURL: supabaseURL, supabaseKey: supabaseKey)
 
